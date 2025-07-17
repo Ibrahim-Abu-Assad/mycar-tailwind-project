d2725ee31a4091f1d535ddabdd54aeebd22d70ac
Ibrahim-Abu-Assad <i6802275@gmail.com>
2025-07-17 16:16:49 +0300

    Initial commit for MyCar project

---
+++ script.js
@@ -0,0 +1,7 @@
+const mbBtn = document.querySelector("#mb-btn");
+const mbmenu = document.querySelector("#mb-menu");
+
+mbBtn.addEventListener('click', function(){
+mbmenu.classList.toggle('hidden');
+});
+
